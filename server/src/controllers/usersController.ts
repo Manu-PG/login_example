1cb11ffe0c0d23de888817f4959243e9d7bee738
Manuel Pedreira <mapedgi@gmail.com>
2024-05-16 17:30:50 +0200

    userModel working

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -8,13 +8,17 @@ export const getUser = (req: Request, res: Response) => {
     .catch((error) => res.status(500).json({ error: (error as Error).message }));
 };
 
-export const createUser = (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
   const { username, password } = req.body;
+  try {
+    const user = await userModel.create({ username, password });
 
-  userModel
-    .create({ username, password })
-    .then((data) => res.json(data))
-    .catch((error) => res.status(500).json({ error: (error as Error).message }));
+    console.log(user);
+    console.log('test', await user.isValidPassword('pass123'));
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: (error as Error).message });
+  }
 };
 
 export const loginUser = (req: Request, res: Response) => {};
--- server/src/models/usersModel.ts
+++ server/src/models/usersModel.ts
@@ -1,14 +1,11 @@
-import mongoose, { Model } from 'mongoose';
+import mongoose, { Error, Model } from 'mongoose';
 import bcrypt from 'bcryptjs';
 import { User } from './interfaces';
 
 interface UserDocument extends User, mongoose.Document {
   createdAt: Date;
   updatedAt: Date;
-  methods: {
-    type: [string];
-    required: true;
-  };
+  isValidPassword: (newPassword: string) => Promise<boolean>;
 }
 
 const Schema = mongoose.Schema;
@@ -37,13 +34,32 @@ const userSchema = new Schema<UserDocument, Model<UserDocument>>(
     toJSON: {
       transform(doc, ret) {
         ret.id = ret._id;
-        //ret.password = '************';
+        delete ret.password;
         delete ret._id;
         delete ret.__v;
       },
     },
   }
 );
+
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(this.password, salt);
+    this.password = passwordHash;
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
+});
+
+userSchema.methods.isValidPassword = async function (newPassword: string) {
+  try {
+    return await bcrypt.compare(newPassword, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
 
 const userModel = mongoose.model<UserDocument>('User', userSchema);
 export default userModel;
